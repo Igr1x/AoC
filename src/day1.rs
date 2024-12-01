795094cb98877a44df1d3cd0a2cb7036eb812bd1
Igr1x <varnavskii.igrix@mail.ru>
2024-12-01 22:10:37 +0300

    Day [#1]: solution improvement

--- src/day1.rs
+++ src/day1.rs
@@ -1,25 +1,45 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 pub fn solve() {
     let result = parse_input_file();
-    let mut step1: i64 = 0;
-    let mut step2: i64 = 0;
     match result {
         Ok((mut vector1, mut vector2)) => {
-            vector1.sort_by(|a, b| a.cmp(b));
-            vector2.sort_by(|a, b| a.cmp(b));
-            for (index, value) in vector1.iter().enumerate() {
-                step1 += (value - vector2[index]).abs();
-                step2 += value * vector2.iter().filter(|&n| *n == *value).count() as i64;
-            }
-            for (index, value) in vector1.iter().enumerate() {}
+            vector1.sort_unstable();
+            vector2.sort_unstable();
+            let step1: i64 = calculate_paths(&vector1, &vector2);
+            let step2: i64 = calculate_multiplications(&vector1, &vector2);
+            println!("Step 1: {}", step1);
+            println!("Step 2: {}", step2);
         }
         _ => {}
     }
+}
 
-    println!("Step 1: {}", step1);
-    println!("Step 2: {}", step2);
+fn calculate_paths(vector1: &[i64], vector2: &[i64]) -> i64 {
+    vector1
+        .iter()
+        .zip(vector2)
+        .map(|(v1, v2)| (v1 - v2).abs())
+        .sum()
+}
+
+fn calculate_multiplications(vector1: &[i64], vector2: &[i64]) -> i64 {
+    let counts = count_elements(vector2);
+    vector1
+        .iter()
+        .map(|&v1| v1 * counts.get(&v1).copied().unwrap_or(0))
+        .sum()
+}
+
+fn count_elements(vec: &[i64]) -> HashMap<i64, i64> {
+    let mut counts = HashMap::new();
+
+    for &value in vec {
+        *counts.entry(value).or_insert(0) += 1;
+    }
+    counts
 }
 
 fn parse_input_file() -> Result<(Vec<i64>, Vec<i64>), io::Error> {
